a195446063640c54e74746b51bb3da7dc2b4d213
cameliapitsilis-lgtm <cameliapitsilis@gmail.com>
2025-10-01 20:57:47 -0400

    DRAWN SELF-PORTRAIT
    Drawing a self-portrait using shapes

--- art-jam/js/script.js
+++ art-jam/js/script.js
@@ -1,24 +1,129 @@
 /**
- * Title of Project
- * Author Name
+ * Cam's Self-Portrait
+ * Camelia Pitsilis
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * ART JAM Project for CART253 A
  */
 
 "use strict";
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * Creating Canvas
 */
 function setup() {
-
+    //Canvas is 500px wide and 700px tall
+    createCanvas(500, 500)
+    noStroke();
 }
 
-
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * Drawing Myself
 */
 function draw() {
+    //Background Colour RGB
+    background(3, 0, 117)
 
+    //Self-Portrait//(x,y,w,h)
+
+    //FACE COLOUR
+    fill(242, 201, 167);
+    rect(90, 100, 350, 400)
+
+    //HAIR//
+    fill(0, 0, 0);
+    rect(160, 20, 200, 20)
+    rect(120, 40, 300, 20)
+    rect(100, 60, 320, 20)
+    rect(80, 80, 340, 20)
+    rect(60, 100, 240, 20)
+    rect(320, 100, 100, 20)//RIGHT BANGS
+    rect(340, 120, 80, 20)//RIGHT BANGS
+    rect(360, 140, 60, 20)//RIGHT BANGS
+    rect(420, 100, 20, 60)//RIGHT BANGS
+    rect(420, 160, 20, 20)//RIGHT BANGS
+    rect(60, 120, 220, 20)
+    rect(60, 140, 200, 20)
+    rect(60, 160, 120, 20)
+    rect(60, 180, 100, 20)
+    rect(60, 200, 80, 20)
+    rect(60, 200, 20, 280)//LENGHT
+    rect(40, 120, 20, 240)
+    rect(80, 200, 20, 300)
+    rect(100, 300, 40, 200)
+    rect(140, 360, 40, 140)
+    rect(340, 420, 40, 80)
+    rect(380, 400, 20, 100)
+    rect(400, 380, 20, 120)
+    rect(420, 320, 20, 200)
+
+
+
+    //FACE//
+    //
+    //NOSE//
+    fill(186, 146, 112);
+    rect(320, 260, 20, 20)
+    rect(300, 280, 40, 20)
+    //RIGHT//
+    rect(420, 180, 20, 40)
+    rect(440, 220, 20, 80)
+    rect(420, 300, 20, 20)
+    rect(400, 320, 20, 60)
+    rect(380, 380, 20, 20)
+    rect(360, 400, 20, 20)
+    //LEFT//
+    rect(140, 300, 20, 40)
+    rect(140, 340, 20, 20)
+    rect(160, 360, 20, 20)
+    rect(180, 380, 20, 20)
+    rect(200, 400, 20, 20)
+    rect(220, 420, 140, 20)//CHIN LINE
+    //NECK//
+    rect(180, 400, 20, 100)
+    rect(320, 420, 20, 80)
+
+    //SHADOWS
+    fill(120, 84, 54);
+    rect(260, 440, 80, 20)
+    rect(280, 460, 60, 20)
+    rect(300, 480, 40, 20)
+
+    //EYES//
+    fill(0, 0, 0);
+    rect(320, 180, 80, 20)
+    rect(360, 180, 40, 60)
+    rect(200, 180, 80, 20)
+    rect(240, 180, 40, 60)
+
+    //EYE HIGHLIGHT
+    fill(255, 255, 255);
+    rect(360, 200, 20, 20)
+    rect(240, 200, 20, 20)
+
+    //MOUTH//
+    fill(191, 2, 71);
+    rect(260, 320, 80, 20)
+    rect(260, 340, 20, 20)
+    rect(280, 360, 60, 20)
+
+    //TEETH
+    fill(255, 255, 255);
+    rect(280, 340, 60, 20)
+
+    //BLUSH
+    fill(255, 115, 190);
+    rect(200, 260, 40, 20)
+    rect(380, 260, 40, 20)
+
+    //EAR//
+    fill(186, 146, 112);
+    rect(120, 240, 20, 20)
+
+    //EARRING
+    fill(255, 238, 84);
+    rect(120, 280, 20, 40)
+
+    //NECKLACE
+    fill(255, 238, 84);
+    rect(180, 480, 160, 20)
 }
